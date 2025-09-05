20280497ac9ccf3d252e02e108733f92ef7e2f81
Pedro Henrique <ph3116633@gmail.com>
2025-09-04 22:03:38 -0300

    Repository-Projetos-Faculdade

--- Projeto 14.cpp
+++ Projeto 14.cpp
@@ -4,19 +4,19 @@
 void incluir(){
 	FILE *pont_arq;
 	char lanche[20];
-	pont_arq=fopen("lanche.txt", "a");
+	pont_arq=fopen("lanche.txt","a");
 	if(pont_arq==NULL)
 	   printf("Erro na abertura do arquivo");
 	printf("Digite o nome do lanche:");
-	scanf("%s", &lanche);
+	scanf("%s",&lanche);
 	fprintf(pont_arq,"%s",lanche);
-	fprintf(pont_arq, "%s" "\n");
+	fprintf(pont_arq, "%s", "\n");
 	fclose(pont_arq);
 	}
 void consultar(){
 	FILE *pont_arq;
 	char texto[20];
-	pont_arq=fopen("lanche.txt", "r");
+	pont_arq=fopen("lanche.txt","r");
 	while(fgets(texto,20,pont_arq)!=NULL)
 	printf("%s",texto);
 	fclose(pont_arq);
@@ -37,7 +37,7 @@ main(){
 		if(opcao==1) incluir();
 		if(opcao==2) consultar();
 		printf("Deseja continuar?");
-		scanf("%s",&continua);
+		scanf(" %c",&continua);
 		
 	}
 }
